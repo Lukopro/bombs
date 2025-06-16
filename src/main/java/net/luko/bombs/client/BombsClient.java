ceec120cc7d7c7243b94b280914dabc8c70aaca9
Luko <ljstenerson@gmail.com>
2025-06-16 18:31:56 -0500

    Each tier dynamite has different texture. Modified bombs have new texture for each tier.

--- src/main/java/net/luko/bombs/client/BombsClient.java
+++ src/main/java/net/luko/bombs/client/BombsClient.java
@@ -3,6 +3,7 @@ package net.luko.bombs.client;
 import net.luko.bombs.Bombs;
 import net.luko.bombs.item.ModItems;
 import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceLocation;
 
 public class BombsClient {
@@ -11,14 +12,20 @@ public class BombsClient {
     }
 
     private static void registerItemProperties(){
+
+
         ItemProperties.register(ModItems.DYNAMITE.get(),
-                new ResourceLocation(Bombs.MODID, "modifier"),
+                new ResourceLocation(Bombs.MODID, "index"),
                 (pStack, pLevel, pEntity, pSeed) -> {
-                    if(pStack.hasTag() && pStack.getTag().contains("Modifiers")){
-                        return 1.0F;
+                    float index = 1.0F;
+                    if(pStack.hasTag() && pStack.getTag().contains("Tier", Tag.TAG_INT)){
+                        index += (float) pStack.getTag().getInt("Tier");
+                        index--;
                     }
-                    return 0.0F;
+                    if(pStack.hasTag() && pStack.getTag().contains("Modifiers")){
+                        index += 5.0F;
+                    }
+                    return index;
                 });
     }
-
 }

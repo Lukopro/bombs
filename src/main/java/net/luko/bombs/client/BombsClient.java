66b077e0ef1befec3b291e97fb151c8f82d48771
Luko <ljstenerson@gmail.com>
2025-06-21 13:34:13 -0500

    Item model predicate logic moved to a Util

--- src/main/java/net/luko/bombs/client/BombsClient.java
+++ src/main/java/net/luko/bombs/client/BombsClient.java
@@ -3,6 +3,7 @@ package net.luko.bombs.client;
 import net.luko.bombs.Bombs;
 import net.luko.bombs.data.ModDataComponents;
 import net.luko.bombs.item.ModItems;
+import net.luko.bombs.util.BombTextureUtil;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceLocation;
@@ -18,15 +19,7 @@ public class BombsClient {
         ItemProperties.register(ModItems.DYNAMITE.get(),
                 ResourceLocation.fromNamespaceAndPath(Bombs.MODID, "index"),
                 (pStack, pLevel, pEntity, pSeed) -> {
-                    float index = 1.0F;
-                    if(pStack.has(ModDataComponents.TIER.get())){
-                        index += (float) pStack.getOrDefault(ModDataComponents.TIER.get(), 1);
-                        index--;
-                    }
-                    if(pStack.has(ModDataComponents.MODIFIERS.get()) && !pStack.getOrDefault(ModDataComponents.MODIFIERS.get(), List.of()).isEmpty()){
-                        index += 5.0F;
-                    }
-                    return index;
+                    return BombTextureUtil.getTextureIndex(pStack);
                 });
     }
 }
